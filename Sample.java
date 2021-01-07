cd71506840c0fa961c3c1468315bfe451c3b4beb
keisuke14 <kamatho.king13@gmail.com>
2021-01-07 13:17:34 +0000

    変数宣言

--- Sample.java
+++ Sample.java
@@ -1,5 +1,18 @@
-  class Sample{
+  /*サンプルププログラム*/
+  public class Sample{
     public static void main(String[] args){
-      System.out.println("Hello");
+      int age = 20;
+      System.out.println("私の年齢は" + age);
+      age = 31;
+      System.out.println("いや、本当の年齢は" + age);
+
+      int a = 3; int b = 5;
+      int c = a*b;
+      System.out.println("縦幅3横幅5の長方形の面積は" + c);
+
+      int d ; int e;
+      d = 20; e = d + 5;
+      System.out.println(d);
+      System.out.println(e);
     }
   }
