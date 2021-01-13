dfcd8d3a38c6ac07798d1fbe7d8ae147e7c560e5
keisuke14 <kamatho.king13@gmail.com>
2021-01-13 10:35:51 +0000

    演算時の型変換

--- Sample.java
+++ Sample.java
@@ -14,5 +14,26 @@
       d = 20; e = d + 5;
       System.out.println(d);
       System.out.println(e);
+
+      int f;
+      f = 100;
+      /*fの内容が1増える*/
+      f++;
+      System.out.println(f);
+      /*intが強制的な型変換を指示するキャスト演算子*/
+      int year = (int)3.2;
+      System.out.println(year);
+
+      /*2(int型)を2.0(double型)に変換*/
+      double g = 8.5 / 2;
+      /*5(int型)を5L(long型)に変換*/
+      long l = 5 + 2L;
+      System.out.println(g);
+      System.out.println(l);
+
+      /*23(int型)が"23"(String型)に変換されて連結される*/
+      String msg = "私の年齢は" + 23;
+      System.out.println(msg);
+
     }
   }
