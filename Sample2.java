cdf7806a8402f9d6891d2152bcfd83478789a930
keisuke14 <kamatho.king13@gmail.com>
2021-01-28 07:03:21 +0000

    配列

--- Sample2.java
+++ Sample2.java
@@ -1,4 +1,12 @@
   public class Sample2 {
     public static void main (String[] args) {
+      /*int型の要素を代入できる型*/
+      int[] scores;
+      /*new演算子　指定された型の要素を[]内に指定された数だけ作成*/
+      scores = new int[5];
+      /*配列の要素の数を調べるコード*/
+      int num = scores.length;
+      System.out.println("要素の数:" + num);
+
     }
   }
